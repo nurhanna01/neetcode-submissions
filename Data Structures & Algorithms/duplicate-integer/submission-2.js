9c71fa6dd2e15271e3de2c0389a56de14e8f17df
Nur Hanna <54818992+nurhanna01@users.noreply.github.com>
2026-05-08 10:12:31 +0700

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let a = {}
+        for(let i=0;i<nums.length;i++){
+            const key=nums[i].toString()
+            if(!a[key]){
+                a[key]=key
+            } else {
+                return true
+            }
+        }
+    return false
+    }
+}
